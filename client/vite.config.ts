9ed307807efcecb8ddc007e959468f5c4819e7f4
watermelonPop <SK21007@Tamu.edu>
2026-02-15 22:42:44 -0500

    more pdf fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import commonjs from '@rollup/plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
