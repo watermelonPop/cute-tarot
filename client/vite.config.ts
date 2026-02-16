1d56e0450e8ef4e71db0de046ea130b768f1f7c8
watermelonPop <SK21007@Tamu.edu>
2026-02-15 22:37:30 -0500

    more pdf fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,5 +20,10 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['html2pdf.js'], // pre-bundle this package
+  },
+  build: {
+    rollupOptions: {
+      plugins: [commonjs()],
+    },
   },
 })
