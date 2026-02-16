734a810cf3ee0d53e2f59b1049e282bd5bb71a64
watermelonPop <SK21007@Tamu.edu>
2026-02-15 21:07:52 -0500

    making changes for deployment

--- server/index.ts
+++ server/index.ts
@@ -30,6 +30,4 @@ app.get('/health', (_req, res) => {
   res.json({ status: 'ok' })
 })
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-})
+export default app;
