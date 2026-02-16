7d9659bb8c35b6c8abced6d99e7273f09400217b
watermelonPop <SK21007@Tamu.edu>
2026-02-16 15:40:05 -0500

    more server troubleshooting

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,10 +1,5 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-
-const connectionString = process.env.DATABASE_URL!;
-
-const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -12,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
